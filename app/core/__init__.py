ba2e59945bc431aec9895dec5cd5f1e4285a59e5
coolity <coolitydev@gmail.com>
2025-03-26 17:38:15 +0300

    Init database helper

---
+++ app/core/__init__.py
@@ -0,0 +1,6 @@
+__all__ = (
+    "DatabaseHelper",
+    "db_helper"
+)
+
+from .db_helper import DatabaseHelper, db_helper
---
+++ app/core/db_helper.py
@@ -0,0 +1,37 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+
+from config import settings
+from core.models import Base
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, echo: bool, pool_size: int, max_overflow: int) -> None:
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(bind=self.engine)
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+    async def database_engine(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+
+db_helper = DatabaseHelper(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow
+)
