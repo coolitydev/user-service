887500f5bbc25f7110880a060a7dc3c719375a9d
coolity <coolitydev@gmail.com>
2025-03-26 17:34:48 +0300

    Add created and updated mixin

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,5 +1,23 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+
+class CreatedAtMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        server_default=func.now()
+    )
+
+
+class UpdatedAtMixin:
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
