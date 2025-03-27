7822609c85892ec149db1c1f36eab6afdb67d93b
coolity <coolitydev@gmail.com>
2025-03-27 17:20:22 +0300

    Add user endpoints

--- app/api/v1/user/router.py
+++ app/api/v1/user/router.py
@@ -1,10 +1,29 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from api.v1.user.dependencies import get_user_service
+from database.schemas.user import UserRead, UserCreate
+from services import UserService
 
 router = APIRouter(
     prefix="/user",
     tags=["User"]
 )
 
-@router.get("/")
-async def get_user():
-    pass
+
+@router.post("/", response_model=UserRead)
+async def create_user(
+        user_data: UserCreate,
+        user_service: Annotated[UserService, Depends(get_user_service)],
+):
+    user = await user_service.create(user_data=user_data)
+    return user
+
+
+@router.get("/{telegram_id}", response_model=UserRead)
+async def get_user(
+        telegram_id: int,
+        user_service: Annotated[UserService, Depends(get_user_service)]
+):
+    return await user_service.get_by_telegram_id(telegram_id=telegram_id)
