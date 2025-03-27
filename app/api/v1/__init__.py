24fd11b63599fc628e5946326f08647b50e8f73b
coolity <coolitydev@gmail.com>
2025-03-27 04:24:01 +0300

    Fix imports

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from user.router import router as user_router
+from api.v1.user.router import router as user_router
 
 router = APIRouter(prefix="/v1")
 router.include_router(user_router)
