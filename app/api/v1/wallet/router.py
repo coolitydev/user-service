225c6bfd879982d383ebc45e6c51e1f5b5981b2e
coolity <coolitydev@gmail.com>
2025-03-28 05:36:39 +0300

    Add wallet endpoints

--- app/api/v1/wallet/router.py
+++ app/api/v1/wallet/router.py
@@ -1,6 +1,29 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from api.v1.wallet.dependencies import get_wallet_service
+
+from database.schemas.wallet import WalletRead
+from services.wallet import WalletService
 
 router = APIRouter(
     prefix="/wallet",
     tags=["Wallet"]
 )
+
+
+@router.post("/create/{telegram_id}", response_model=WalletRead)
+async def create_wallet(
+        telegram_id: int,
+        wallet_service: Annotated[WalletService, Depends(get_wallet_service)],
+):
+    return await wallet_service.create(telegram_id=telegram_id)
+
+
+@router.get("/get/{telegram_id}")
+async def get_wallet(
+        telegram_id: int,
+        wallet_service: Annotated[WalletService, Depends(get_wallet_service)]
+):
+    return await wallet_service.get_by_telegram_id(telegram_id=telegram_id)
