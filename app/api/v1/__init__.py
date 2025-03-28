1d3cb4383f5120dd8561f00a82255c7f39b91c17
coolity <coolitydev@gmail.com>
2025-03-28 05:36:52 +0300

    Include wallet router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from api.v1.user.router import router as user_router
+from api.v1.wallet.router import router as wallet_rouer
 
 router = APIRouter(prefix="/v1")
 router.include_router(user_router)
+router.include_router(wallet_rouer)
